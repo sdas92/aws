0c2c7174fca12d7302a2b3b34c412ab1fe62cacf
Somnath Das <54442341+sdas92@users.noreply.github.com>
2019-08-24 07:27:09 +0530

    Create insert_record.py
    Python program to insert or update a record with key-value pair into existing DynamoDB table.

---
+++ DynamoDB/insert_record.py
@@ -0,0 +1,22 @@
+#******************Author:Somnath Das**********************
+#******AWS-CLI should be configured prior to run this******
+
+import boto3
+
+client = boto3.client('dynamodb')
+
+# Table (bookstore) was already created with Partition Key 'category' and Sort Key as 'bookid' which is unique.
+response = client.put_item(
+  TableName='bookstore',
+  Item={
+    #This is the primary key which contains the partition key and sort key
+    'category': {'S': 'sci-fi'},
+    'bookid': {'S': 'bk002' },
+    #Now the other columns to be inserted into the table
+    'bookname': {'S': 'Ghanada Samagra'},
+    'author': {'S': 'Pemendra Mitra'},
+    'yop': {'N': '1980'},
+    'language': {'S': 'Bengali'},
+    'country': {'S': 'India'}
+  }
+)
