aec152c01ce1be09efca3474f545b3fb5a02519e
Somnath Das <54442341+sdas92@users.noreply.github.com>
2019-08-25 16:29:36 +0530

    Deletes a S3 bucket
    Deletes a S3 bucket from your account. Please make sure that the bucket is empty.

---
+++ s3/s3DeleteBucket.py
@@ -0,0 +1,23 @@
+#********************Author: Somnath Das***********************
+#********Please ensure awscli is configured properly***********
+
+import boto3
+#import logging
+from botocore.exceptions import ClientError
+from colorama import Fore, Back, Style
+
+s3_client = boto3.client('s3')
+
+#Delete your bucket...
+def deleteBucket(bucket_name):
+  s3_client.delete_bucket(Bucket=bucket_name)
+#Method definition ends.
+
+bucket_name = input("Enter your bucket name: ")
+try:
+  deleteBucket(bucket_name)
+  print("Deleted your bucket: !" % bucket_name)
+except ClientError as e:
+  print(e)
+  input("Wait till exit: ")
+
